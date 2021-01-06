d67d428b5665c407cd3329d40cf995f29f1a8ea2
jarell Boone <jarellboone4@gmail.com>
2021-01-05 20:32:43 -0500

    changes made to html and java file

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $(".button").on("click", function({
+       //distinguish which save button was clicked by using window obj THIS  $(this)
+       //grab value from our textarea
+
+     
+    })
+
+
+})
+
+{/* <div row id="9am">
+    <div col >9am</div>
+    <textarea col></textarea>
+    <buton col>save</buton>
+<div> */}
