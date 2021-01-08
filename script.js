e805369d1e52ee75429dbf50154b6cbe9d2996b1
jarell Boone <jarellboone4@gmail.com>
2021-01-07 23:34:26 -0500

    I updated time and date and worked om JS

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-$(document).ready(function(){
-    $(".button").on("click", function({
-       //distinguish which save button was clicked by using window obj THIS  $(this)
-       //grab value from our textarea
+function currenttime() {
+    $("#currentDay").html(moment().format('MMMM Do YYYY, h:mm:ss a')); 
+    }
+    setInterval(currenttime, 500);
+  
+//    $(document).ready(function(){
+//     $(".button").on("click", function({
+//        //distinguish which save button was clicked by using window obj THIS  $(this)
+//        //grab value from our textarea
 
-     
-    })
+//     //  $(this)
+//     })
 
 
-})
+// })
 
-{/* <div row id="9am">
+/* <div row id="9am">
     <div col >9am</div>
     <textarea col></textarea>
     <buton col>save</buton>
-<div> */}
+<div> */
