e623d7f2f405eb96ca68a3fdb763c63f02ee681f
jarell Boone <jarellboone4@gmail.com>
2021-01-09 00:03:16 -0500

    created functin for local storage in js

--- script.js
+++ script.js
@@ -3,7 +3,40 @@ function currenttime() {
     }
     setInterval(currenttime, 500);
   
-//    $(document).ready(function(){
+   $("#9amButton").on("click",()=> {
+        
+      let textArea =  document.getElementById("B").value;
+      let StringifytextArea = JSON.stringify(textArea)
+      localStorage.setItem("textArea", StringifytextArea )
+    })
+
+    $("#10amButton").on("click",()=> {
+        
+        let textArea =  document.getElementById("A").value;
+        let StringifytextArea = JSON.stringify(textArea)
+        localStorage.setItem("textArea", StringifytextArea )
+      })
+
+      $("#11amButton").on("click",()=> {
+        
+        let textArea =  document.getElementById("C").value;
+        let StringifytextArea = JSON.stringify(textArea)
+        localStorage.setItem("textArea", StringifytextArea )
+      })
+      $("#12pmButton").on("click",()=> {
+        
+        let textArea =  document.getElementById("D").value;
+        let StringifytextArea = JSON.stringify(textArea)
+        localStorage.setItem("textArea", StringifytextArea )
+      })
+      $("#1pmButton").on("click",()=> {
+        
+        let textArea =  document.getElementById("E").value;
+        let StringifytextArea = JSON.stringify(textArea)
+        localStorage.setItem("textArea", StringifytextArea )
+      })
+
+// $(document).ready(function(){
 //     $(".button").on("click", function({
 //        //distinguish which save button was clicked by using window obj THIS  $(this)
 //        //grab value from our textarea
